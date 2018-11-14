8ed18165bd8f18d7b598114f82bac0831657539f
benjaminreis <benjaminreis@gmail.com>
2018-11-13 20:56:27 -0600

    homework1

--- COP2210/Program.cs
+++ COP2210/Program.cs
@@ -8,6 +8,31 @@ namespace COP2210
         {
             Console.WriteLine("Hello World!");
 
+            var balloon = new Balloon("ben balloon", 150);
+            var balloon2 = new Balloon("second balloon", -50);
+
+            Console.WriteLine($"Balloon name: {balloon.getName()}  Height: {balloon.getAltitude()}");
+            Console.WriteLine($"Balloon name: {balloon2.getName()}  Height: {balloon2.getAltitude()}");
+
+
+            balloon.ascendTo(200);
+            balloon2.adjustAltitude(100);
+
+            Console.WriteLine($"Balloon name: {balloon.getName()}  Height: {balloon.getAltitude()}");
+            Console.WriteLine($"Balloon name: {balloon2.getName()}  Height: {balloon2.getAltitude()}");
+
+            balloon.adjustAltitude(-50);
+            balloon2.ascendTo(balloon.getAltitude());
+
+
+            Console.WriteLine($"Balloon name: {balloon.getName()}  Height: {balloon.getAltitude()}");
+            Console.WriteLine($"Balloon name: {balloon2.getName()}  Height: {balloon2.getAltitude()}");
+
+            balloon.ascendTo(balloon.getAltitude() * 3);
+            balloon2.ascendTo(balloon2.getAltitude() - 200);
+
+            Console.WriteLine($"Balloon name: {balloon.getName()}  Height: {balloon.getAltitude()}");
+            Console.WriteLine($"Balloon name: {balloon2.getName()}  Height: {balloon2.getAltitude()}");
         }
     }
 }
