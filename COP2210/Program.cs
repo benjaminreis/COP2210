17ceed4521f4335c5c5745c0615701599a5401ca
benjaminreis <benjaminreis@gmail.com>
2018-11-13 20:33:42 -0600

    initial commit

---
+++ COP2210/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COP2210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
