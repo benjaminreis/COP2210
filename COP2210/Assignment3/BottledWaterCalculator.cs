0636c58005ce812b2d6e615b938e67cf810db833
benjaminreis <benjaminreis@gmail.com>
2018-11-14 21:02:24 -0600

    started bottledWaterCalculator class

---
+++ COP2210/Assignment3/BottledWaterCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace COP2210.Assignment3
+{
+    public class BottledWaterCalculator
+    {
+        public BottledWaterCalculator(string CountryName, int CountryPopulation, double NumberTimesCircleEarth, 
+                                      double AverageBottleLength, double AverageBottleVolume)
+        {
+            this._CountryName = CountryName;
+            this._CountryPopulation = CountryPopulation;
+            this._AverageBottleLength = AverageBottleLength;
+            this._AverageBottleVolume = AverageBottleVolume;
+            this._NumberTimesCircleEarth = NumberTimesCircleEarth;
+        }
+
+
+        private string _CountryName;
+        private int _CountryPopulation;
+        private double _NumberTimesCircleEarth;
+        private double _AverageBottleLength;   //  Inches
+        private double _AverageBottleVolume;  //Fl ounces
+
+
+
+    }
+}
