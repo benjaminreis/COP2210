2e75744806ba52f35038fc4ed845a0439423140a
benjaminreis <benjaminreis@gmail.com>
2018-11-14 21:23:28 -0600

    working on class methods

--- COP2210/Assignment3/BottledWaterCalculator.cs
+++ COP2210/Assignment3/BottledWaterCalculator.cs
@@ -3,7 +3,7 @@ namespace COP2210.Assignment3
 {
     public class BottledWaterCalculator
     {
-        public BottledWaterCalculator(string CountryName, int CountryPopulation, double NumberTimesCircleEarth, 
+        public BottledWaterCalculator(string CountryName, int CountryPopulation, double NumberTimesCircleEarth,
                                       double AverageBottleLength, double AverageBottleVolume)
         {
             this._CountryName = CountryName;
@@ -20,7 +20,83 @@ namespace COP2210.Assignment3
         private double _AverageBottleLength;   //  Inches
         private double _AverageBottleVolume;  //Fl ounces
 
+        public string CountryName { 
+            get 
+            { 
+                return _CountryName; 
+            } 
+            set 
+            { 
+                _CountryName = value;
+            } 
+        }
 
+        public int CountryPopulation
+        {
+            get
+            {
+                return _CountryPopulation;
+            }
+            set
+            {
+                _CountryPopulation = value;
+            }
+        }
+
+
+        public double NumberTimesCircleEarth
+        {
+            get
+            {
+                return _NumberTimesCircleEarth;
+            }
+            set
+            {
+                _NumberTimesCircleEarth = value;
+            }
+        }
+
+        public double AverageBottleLength
+        {
+            get
+            {
+                return _AverageBottleLength;
+            }
+            set
+            {
+                _AverageBottleLength = value;
+            }
+        }
+
+        public double AverageBottleVolume
+        {
+            get
+            {
+                return _AverageBottleVolume;
+            }
+            set
+            {
+                _AverageBottleVolume = value;
+            }
+        }
+
+
+        internal double NumberOfBottlesUsed(double AverageBottleLength, double NumberTimesCircleEarth)
+        {
+            double NumberOfBottles = new double();
+
+            NumberOfBottles = NumberTimesCircleEarth / AverageBottleLength;
+
+            return NumberOfBottles;
+        }
+
+
+        internal double AverageWaterPerPerson()
+        {
+            double AverageWaterPerPerson = new double();
+
+            return AverageWaterPerPerson;
+        }
 
     }
 }
