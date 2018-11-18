0da3169657921ff22a740511f150efc704ae1282
benjaminreis <benjaminreis@gmail.com>
2018-11-17 23:21:31 -0600

    ElapsedTimeCalculator Class

--- COP2210/Assignment4/ElapsedTimeCalculator.cs
+++ COP2210/Assignment4/ElapsedTimeCalculator.cs
@@ -3,8 +3,17 @@ namespace COP2210.Assignment4
 {
     internal class ElapsedTimeCalculator
     {
-        public ElapsedTimeCalculator()
+        private int _StartTime;
+        private int _EndTime;
+
+        internal ElapsedTimeCalculator(int StartTime, int EndTime)
         {
+            this._EndTime = EndTime;
+            this._StartTime = StartTime;
         }
+
+
+
+
     }
 }
