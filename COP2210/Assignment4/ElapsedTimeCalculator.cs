61531ad07ced73d8664ed7a613413be3918de4ee
benjaminreis <benjaminreis@gmail.com>
2018-11-18 22:05:42 -0600

    more

--- COP2210/Assignment4/ElapsedTimeCalculator.cs
+++ COP2210/Assignment4/ElapsedTimeCalculator.cs
@@ -5,11 +5,14 @@ namespace COP2210.Assignment4
     {
         private int _StartTime;
         private int _EndTime;
+        private DateTime _StartDateTime;
+        private DateTime _EndDateTime;
 
         internal ElapsedTimeCalculator(int StartTime, int EndTime)
         {
             this._EndTime = EndTime;
             this._StartTime = StartTime;
+            CalculateElapsedTime();
         }
 
 
@@ -25,8 +28,35 @@ namespace COP2210.Assignment4
             set { _EndTime = value; }
         }
 
+        internal DateTime StartDateTime { get; set; }
+        internal DateTime EndDateTime { get; set; }
+
+        private void CalculateInputsDateTime()
+        {
+           this._StartDateTime = DateTime.ParseExact(this._StartTime.ToString(), "HHMM", System.Globalization.CultureInfo.CurrentCulture);
+            this._EndDateTime = DateTime.ParseExact(this._EndTime.ToString(), "HHMM", System.Globalization.CultureInfo.CurrentCulture);
+
+        }
 
 
+        internal int TotalElapsedTimeMinutes()
+        {
+            //todo ben implement later
+        }
+
+        internal int TotalElapsedTimeSeconds()
+        {
+            //todo ben implement later
+        }
+
+
+        internal TimeSpan CalculateElapsedTime()
+        {
+            var elapsed = this._EndDateTime - this._StartDateTime;
+
+            return elapsed;
+
+        }
 
 
     }
