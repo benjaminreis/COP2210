5db13c6a401706074f603ebdb231d8e051b4438e
benjaminreis <benjaminreis@gmail.com>
2018-11-22 06:44:47 -0600

    new commissionCalculator class

--- COP2210/Assignment5/CommissionCalculator.cs
+++ COP2210/Assignment5/CommissionCalculator.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 namespace COP2210.Assignment5
 {
-    public class CommissionCalculator
+    internal class CommissionCalculator
     {
-        public CommissionCalculator()
+        internal CommissionCalculator(string SalesPersonsName, decimal TotalMonthlySales, int YearsService, string Rank)
         {
+            this.SalesPersonsName = SalesPersonsName;
+            this.TotalMonthlySales = TotalMonthlySales;
+            this.YearsService = YearsService;
+            this.Rank = Rank;
+
         }
+
+
+        internal string SalesPersonsName;
+        internal decimal TotalMonthlySales;
+        internal int YearsService;
+        internal string Rank;  //“1” = Apprentice, “2” = Junior and “3” = Senior.
+
+        internal string getData()
+        {
+
+
+            return "";
+        }
+
+        internal double computerCommission()
+        {
+            return 0.0;
+        }
+
+
     }
 }
