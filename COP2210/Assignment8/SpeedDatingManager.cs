c574f8eb1cdb907770692b77233f90b493a8af82
benjaminreis <benjaminreis@gmail.com>
2018-12-03 19:53:11 -0600

    new SpeedDatingManager class

---
+++ COP2210/Assignment8/SpeedDatingManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace COP2210.Assignment8
+{
+    internal class SpeedDatingManager : IManager
+    {
+        internal SpeedDatingManager()
+        {
+        }
+
+        void IManager.Assignment()
+        {
+
+
+        }
+    }
+}
