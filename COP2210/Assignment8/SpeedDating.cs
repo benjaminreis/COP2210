629f845f4f54656e3a16e0c88ef795ab7a71f33a
benjaminreis <benjaminreis@gmail.com>
2018-12-03 19:48:42 -0600

    SpeedDating class

---
+++ COP2210/Assignment8/SpeedDating.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace COP2210.Assignment8
+{
+
+    /**
+     * A class to give students experience using loops.  This class
+     * creates and manipulates objects of Greg's Date class.
+     */
+    public class SpeedDating
+    {
+        // Note: this class has no instance variables!
+
+        /**
+         * Create a SpeedDating object
+         */
+        public SpeedDating()
+        { }
+        // Note to students: Constructor takes no parameters - this is known
+        // as a "default" constructor.  This one also has an empty body
+
+        /**
+         * Computes and prints the day of the week on which New Year's Day will
+         * fall for 10 consecutive years
+         * @param theYear the first of the 10 consecutive years
+         */
+        public void auldLangSynch(int theYear)
+        {
+            // TO DO: write body of this method here
+
+        }
+
+        /**
+         * Computes and returns the Date on which Election Day will fall 
+         * in the USA for a given year
+         *
+         * NOTE: By law, Election Day is the first Tuesday after the first
+         * Monday in November
+         *
+         * @param year the year for which to compute the date of Election Day
+         * @return the Date of Election Day for the specified year
+         */
+        public Date pollDancer(int year)
+        {
+            // TO DO: write body of method here
+
+            // bogus return value so class skeleton will compile and execute
+            return null;
+        }
+
+        /**
+         * Computes and returns the Date that occurs "half way" between two Dates.
+         * 
+         * @param startDate the earlier of the two Dates
+         * @param endDate the later of the two Dates
+         * @return the Date midway between startDate and endDate 
+         */
+        public Date getHalfwayDate(Date startDate, Date endDate)
+        {
+            // TO DO: write remainder of body of this method here
+
+            // bogus return value so class skeleton will compile and execute
+            return null;
+        }
+    }
+}
