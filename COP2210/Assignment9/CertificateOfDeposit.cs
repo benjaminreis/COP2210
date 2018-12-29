e1b56a6755e749f5cbd4a8620f1d59c5f42d579d
benjaminreis <benjaminreis@gmail.com>
2018-12-28 18:44:19 -0600

    added an enum.

--- COP2210/Assignment9/CertificateOfDeposit.cs
+++ COP2210/Assignment9/CertificateOfDeposit.cs
@@ -11,14 +11,17 @@ namespace COP2210.Assignment9
         private decimal Principal;
         private decimal InterestRate;
         private int Maturity;
-        private int CompoundingMode;
+        private CompoundingMode CompoundingFrequency;  //times compounding per year.  Quartery = 4
 
+        enum CompoundingMode : int { Quarterly = 4, Monthly = 12, Daily = 365 };
 
 
         internal decimal ComputeAccumulatedValue(int Year)
         {
             //TODO BEN write method to Compute value of loan through a given year from now.  
             //this will computer principal plus accumulated interest
+
+            var interest = Principal * 
             return 0.0M;
         }
     }
