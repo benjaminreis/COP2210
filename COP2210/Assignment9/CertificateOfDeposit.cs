41762c9c1f3d0872f4b341580d1d30834c53002c
benjaminreis <benjaminreis@gmail.com>
2018-12-31 21:14:58 -0600

    playing with interest calculator

--- COP2210/Assignment9/CertificateOfDeposit.cs
+++ COP2210/Assignment9/CertificateOfDeposit.cs
@@ -8,14 +8,14 @@ namespace COP2210.Assignment9
             this._Principal = Principal;
             this._InterestRate = InterestRate;
             this._Maturity = Maturity;
-            this._CompoundingFrequency = CompoundingFrequency;
+            this._CompoundingFrequency = (int)CompoundingFrequency;
         }
 
 
         private decimal _Principal;
         private decimal _InterestRate;
         private int _Maturity;
-        private CompoundingMode _CompoundingFrequency;  //times compounding per year.  Quartery = 4
+        private int _CompoundingFrequency;  //times compounding per year.  Quartery = 4
 
         internal enum CompoundingMode : int { Quarterly = 4, Monthly = 12, Daily = 365 };
 
@@ -25,7 +25,8 @@ namespace COP2210.Assignment9
             //TODO BEN write method to Compute value of loan through a given year from now.  
             //this will computer principal plus accumulated interest
 
-            var interest = Principal * 
+            var val = this._Principal * (1 + (this._InterestRate / this._CompoundingFrequency));
+            var interest = Math.Pow((double)val, (this._CompoundingFrequency * (DateTime.Now.Year - Year)));
             return 0.0M;
         }
     }
